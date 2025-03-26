87eeefb404cc8efc282d7c74d1f24773cc40b995
Fernando-Sanz <76251393+Fernando-Sanz@users.noreply.github.com>
2025-03-26 02:52:33 +0100

    Add CMakeLists.txt and main
    The project compiles but fails because a problem with the localization of the assets.

---
+++ tests/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "VulkanApplication.hpp"
+
+
+int main() {
+	VulkanApplication& app = VulkanApplication::getInstance();
+
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << "CAUGHT ERROR: " << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
